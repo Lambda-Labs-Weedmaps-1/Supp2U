64b0e7734314c700ff89dafd3b30d291d6a7597a
zechdrink <zechdrink@aol.com>
2019-08-28 18:11:14 -0700

    added password requirements for users

--- db/migrate/20190828180025_users.rb
+++ db/migrate/20190828180025_users.rb
@@ -3,6 +3,7 @@ class Users < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username, index: {unique: true}
       t.string :email, index: {unique: true}
+      t.string :password, index: {unique: true}
       t.string :wallet
       t.boolean :is_admin, default: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -67,11 +67,13 @@ ActiveRecord::Schema.define(version: 2019_08_28_180025) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
     t.string "wallet"
     t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["password"], name: "index_users_on_password", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
