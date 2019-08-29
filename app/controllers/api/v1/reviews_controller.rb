20b584350d836ce34cea2292d998e80607c7703c
zechdrink <zechdrink@aol.com>
2019-08-28 20:31:06 -0700

    created a reviews controller, ensured one review per business only, and made minor changes to reviews schema

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,34 @@
 module Api
     module V1
       class ReviewsController < ApplicationController
+
+        def index
+          @reviews = Customer.find(params[:customer_id]).reviews
+  
+          render json: @reviews
+        end
+
+        def create
+          #ensures that a Customer can only leave one review per business to prevent review spamming.
+          @has_review = Customer.find(params[:customer_id]).reviews.where(business_id: review_params[:business_id] )
+          @review = Review.new(review_params)
+
+          if @has_review.length > 0
+            render json: { message: "You have already left a review. Thank you for your feedback.", proof: @has_review }
+          elsif @review.save
+            render json: @review, status: :created
+          else
+            render json: @review.errors, status: :unprocessable_identity
+          end
+        end
+
+        private
+        def review_params
+          #passing through the business_id needs to be handled on the front-end
+          #Going to let the front-end handle and decide the range for the rating
+          params.permit(:customer_id, :business_id, :review, :rating)
+        end
+
       end
     end
   end
--- db/migrate/20190828162021_reviews.rb
+++ db/migrate/20190828162021_reviews.rb
@@ -1,9 +1,10 @@
 class Reviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.integer :user_id
+      t.integer :customer_id
       t.integer :business_id
       t.string :review
+      t.integer :rating
 
       t.timestamps
     end
