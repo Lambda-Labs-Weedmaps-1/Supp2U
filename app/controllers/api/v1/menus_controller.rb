367637ced5899364cf6edbe4bfbe0e3d7e8aaf15
zechdrink <zechdrink@aol.com>
2019-08-28 18:48:50 -0700

    finished basic menu controller actions

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -3,12 +3,31 @@ module Api
       class MenusController < ApplicationController
 
         def index
+          @menus = Menu.all
+
+          render json: @menus
         end
 
         def show
+          @menu = Menu.find(params[:business_id])
+
+          render json: @menu
         end
 
         def create
+          @menu = Menu.new(menu_params)
+
+          if @menu.save
+            render json: @menu, status: :created
+          else
+            render json: @menu.errors, status: :unprocessable_identity
+          end
+        end
+
+        private
+        
+        def menu_params
+          params.permit(:business_id, :name)
         end
 
       end
