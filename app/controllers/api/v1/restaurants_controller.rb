931defa7dbdceb0efda4f4463d3c2f5c43b23ec6
Julian Kohlman <juliankohlman@gmail.com>
2019-09-01 17:27:38 -0400

    removes lib and services directories for cleaner refactoring

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -4,52 +4,25 @@ require 'json'
 module Api
 	module V1
 		class RestaurantsController < ApplicationController
-			include Faraday
-
 			def index
-				# Todo place faraday code here to test requests
-				#* refactor to follow convention once it's working.
-				#? https://www.mobomo.com/2012/03/faraday-one-http-client-to-rule-them-all/
-
 				# build connection
 				conn = Faraday.new(url: 'https://eatstreet.com/publicapi/v1')
-				# headers: {
-				# 	# 'X-Mashape-key' => '86e96159e57f9343'
-				# 	'Content-Type' => 'application/json'
-				# }
 
+				# assign request object to resp variable
 				resp =
 					conn.get('restaurant/search') do |req|
 						req.headers['Content-Type'] = 'application/json'
 						req.headers['X-Access-Token'] = ENV['EATSTREET_KEY']
 						req.params['street-address'] = '80012'
 						req.params['method'] = 'both'
-						# req.body = { query: 'burlingame_restaurants' }.to_json
 					end
 
-				# address = resp.body.address
-				# restaurants = resp.body.restaurants
-
+				# parse the response body and save as data variable
 				data = JSON.parse(resp.body)
 
-				render json: data['restaurants'][1]
-				# @restaurants, @errors = Eatstreet::Restaurants.search
-
-				# render json: restaurants
+				# rendering out 2nd restaurant from dataset
+				render json: data['restaurants'][5]
 			end
 		end
 	end
 end
-
-=begin
-Started GET "/api/v1/restaurants" for ::1 at 2019-09-01 00:58:14 -0400
-Processing by Api::V1::RestaurantsController#index as HTML
-Completed 500 Internal Server Error in 164ms (ActiveRecord: 0.0ms)
-
-
-  
-NameError (uninitialized constant Eatstreet::Restaurants::Request):
-  
-app/services/eatstreet/restaurants.rb:9:in `search'
-app/controllers/api/v1/restaurants_controller.rb:5:in `index'
-=end
--- app/services/eatstreet/restaurant.rb
+++
@@ -1,8 +0,0 @@
-module Eatstreet
-	class Restaurant < Base
-		attr_accessor :name, :streetAddress, :city, :foodTypes, :offersDelivery
-
-		# get request with apiKey or id
-		# https://devdocs.io/ruby~2.6/hash#method-i-fetch
-	end
-end
