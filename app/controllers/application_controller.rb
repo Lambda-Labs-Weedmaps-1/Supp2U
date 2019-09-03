7a8952a03ab4947368ffd54f0cb5744b9092c8b1
sibhat <sibhat.temelso@gmail.com>
2019-09-03 14:13:36 -0700

    🔍 login api return customer or business object depending on user id;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,12 @@ class ApplicationController < ActionController::API
         begin
             @decoded = JsonWebToken.decode(header)
             @current_user = User.find(@decoded[:user_id])
-        rescue => ActiveRecord::RecordNotFound => e
+        rescue ActiveRecord::RecordNotFound => e
             render json: { errors: e.message }, status: :unauthorized
-        rescue => JWT::DecodeError => e
+        rescue JWT::DecodeError => e
             render json: { errors: e.message }, status: :unauthorized   
         end
     end
-end        
     
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
     has_one :customer
     has_one :business
 
-    validates :username, presence: true, uniqueness: true
+    validates :username, uniqueness: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, uniqueness: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+    # validates :password, uniqueness: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 
     def self.from_token_payload payload
         puts payload['sub']
