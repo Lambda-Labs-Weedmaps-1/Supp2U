95398c660e9d373363beecaf5b7e6cb9ee1a6e1e
Henry Neal <henry.neal0@gmail.com>
2019-09-03 13:52:20 -0400

    fixed end of input error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::API
             render json: { errors: e.message }, status: :unauthorized   
         end
     end
-end        
+      
     
 
     private
