94d870b001bf71dfdc3268b84fe2592e78228805
Julian Kohlman <juliankohlman@gmail.com>
2019-09-04 16:39:39 -0400

    specced out business validations

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,1 +1,17 @@
 require 'rails_helper'
+
+# Validations what makes a business a business
+RSpec.describe Business, type: :model do
+	it 'is valid with valid attributes' do
+	end
+	it 'is not valid without a name' do
+	end
+	it 'is not valid without a city' do
+	end
+	it 'is not valid without a state' do
+	end
+	it 'is not valid without a street' do
+	end
+	it 'is not valid without a zipcode' do
+	end
+end
