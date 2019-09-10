5c6bd69ddcc5b0ff14cdb940d650835889eadc51
zechdrink <zechdrink@aol.com>
2019-09-10 10:50:42 -0700

    added basic model tests for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ApplicationRecord
     has_many  :items, through: :order_products
     belongs_to :customer
     belongs_to :business
+
+    validates :customer_id, presence: true
+    validates :business_id, presence: true
+    validates :status, presence: true
 end
