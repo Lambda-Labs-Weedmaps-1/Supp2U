3d2c4c35548206742c7f33f2ca48791748f5961e
Julian Kohlman <juliankohlman@gmail.com>
2019-09-12 16:27:36 -0400

    updates schedule_params with new columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_074139) do
+ActiveRecord::Schema.define(version: 2019_09_12_201534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,18 @@ ActiveRecord::Schema.define(version: 2019_09_10_074139) do
     t.integer "business_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sundayopen"
+    t.string "sundayclose"
+    t.string "mondayopen"
+    t.string "mondayclose"
+    t.string "tuesdayopen"
+    t.string "tuesdayclose"
+    t.string "wednesdayopen"
+    t.string "wednesdayclose"
+    t.string "thursdayopen"
+    t.string "thursdayclose"
+    t.string "fridayopen"
+    t.string "fridayclose"
   end
 
   create_table "users", force: :cascade do |t|
