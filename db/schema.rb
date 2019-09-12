975a772e4c46a990bca97ebe1819ea48e87cf9a7
Julian Kohlman <juliankohlman@gmail.com>
2019-09-12 16:57:27 -0400

    schema stripe updates

--- db/schema.rb
+++ db/schema.rb
@@ -50,8 +50,6 @@ ActiveRecord::Schema.define(version: 2019_09_12_201534) do
     t.boolean "recommended"
     t.string "long"
     t.string "lat"
-    t.string "stripe_token"
-    t.string "stripe_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
