0daac40283ae220f3ec9412324842a4918a362f8
Julian Kohlman <juliankohlman@gmail.com>
2019-09-12 10:23:42 -0400

    debugging activeStorage expectation errors

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe Item, type: :model do
 		it { should validate_presence_of(:item_name) }
 		it { should validate_presence_of(:price) }
 	end
+
+	describe 'validate image' do
+		it { is_expected.to be_an_instance_of(ActiveStorage::Attached::One) }
+	end
 end
