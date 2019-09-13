a265b4b35e48c57ecffd2270ed1b7c66434c2d47
Taylor <wwwaaavvveeerrr@gmail.com>
2019-09-13 00:28:34 -0400

    added tests to check for the contents of the email & if it has a sender/reciver

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "order_mailer" do
+    let(:mail) {described_class.order_mailer(user).deliver_now}
+  end
+
+  it 'renders the subject' do
+  expect(mail.subject).to eq('Your order has been placed')  
+  end
+
+  it 'user has an email' do
+    expect(mail.to).to eq([user.email])
+  end
+
+  it 'sender email' do
+    expect(mail.from).to eq(['noreply@devteam.com'])
+  end
+  
 end
--- spec/mailers/sale_mailer_spec.rb
+++ spec/mailers/sale_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe SaleMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "sale_mailer" do
+    let(:mail) {described_class.sale_mailer(user).deliver_now}
+  end
+
+  it 'renders the subject' do
+  expect(mail.subject).to eq('You have an order.')  
+  end
+
+  it 'user has an email' do
+    expect(mail.to).to eq([user.email])
+  end
+
+  it 'sender email' do
+    expect(mail.from).to eq(['noreply@devteam.com'])
+  end
 end
