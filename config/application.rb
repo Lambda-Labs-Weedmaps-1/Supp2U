7ea8d2c4fbf3708b6fed806b993ecaf49aa1b8a5
Julian Kohlman <juliankohlman@gmail.com>
2019-09-14 15:35:49 -0400

    Updated seeds working with correct associations

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Supp2u
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  
+    config.active_record.belongs_to_required_by_default = false
 
 		config.middleware.insert_before 0, Rack::Cors do
 			allow do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,185 +1,172 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-10.times do |i|
-	username = Faker::Internet.user_name
-	email = Faker::Internet.email
-	password = Faker::Internet.password
-	User.create!(username: username, email: email, password: password)
-end
+# Todo real addresses street, zip, lat - long in a hash
+real_places = {
+	1 => {
+		street: '200 Fillmore Street',
+		zip: 80_206,
+		lat: '39.7197500000',
+		long: '-104.9533472222'
+	},
+	2 => {
+		street: '1201 East Colfax Avenue',
+		zip: 80_218,
+		lat: '39.7401111111',
+		long: '-104.9717555556'
+	},
+	3 => {
+		street: '7655 West Alameda Avenue',
+		zip: 80_226,
+		lat: '39.7116700000',
+		long: '-105.0824200000'
+	},
+	4 => {
+		street: '846 Broadway',
+		zip: 80_203,
+		lat: '39.7299500000',
+		long: '-104.9873805556'
+	},
+	5 => {
+		street: '17001 Lincoln Avenue',
+		zip: 80_134,
+		lat: '39.5361583333',
+		long: '-104.7922444444'
+	},
+	6 => {
+		street: '18901 East Mainstreet',
+		zip: 80_134,
+		lat: '39.5187305556',
+		long: '-104.7697916667'
+	},
+	7 => {
+		street: '17001 Lincoln Avenue',
+		zip: 80_134,
+		lat: '39.5361583333',
+		long: '-104.7922444444'
+	},
+	8 => {
+		street: '3400 Navajo Street',
+		zip: 80_211,
+		lat: '39.7645611111',
+		long: '-105.0039361111'
+	},
+	9 => {
+		street: '3160 Tejon Street',
+		zip: 80_211,
+		lat: '39.5361583333',
+		long: '-104.7922444444'
+	},
+	10 => {
+		street: '1919 Blake Street',
+		zip: 80_202,
+		lat: '39.7538500000',
+		long: '-104.9953900000'
+	}
+}
 
-Business.create!(
-	user_id: 10,
-	name: "Angelo's Taverna",
-	website: 'angelosdenver.com',
-	city: 'Denver',
-	state: 'CO',
-	street: '620 E 6th Ave',
-	zipcode: 80_203,
-	theme: 'American',
-	description: "Angelo's Yo !",
-	lat: '39.725465',
-	long: '-104.979174'
-)
+# Cleanup Existing User Data
+User.delete_all
 
-Business.create!(
-	user_id: 2,
-	name: 'Lowdown Brewery',
-	website: 'angelosdenver.com',
-	city: 'Denver',
-	state: 'CO',
-	street: '800 Lincoln St',
-	zipcode: 80_203,
-	theme: 'American',
-	description: 'Low down place for Low down people!',
-	lat: 39.729261,
-	long: -104.985802
-)
-
-Business.create!(
-	user_id: 3,
-	name: 'Ivy On 7th',
-	website: 'ivyon7th.com',
-	city: 'Denver',
-	state: 'CO',
-	street: '410 E 7th Ave, Denver',
-	zipcode: 80_203,
-	theme: 'American',
-	description: "It's IVY !!!",
-	lat: 39.727066,
-	long: -104.981963
-)
-
-Business.create!(
-	user_id: 6,
-	name: Faker::Name.name,
-	website: Faker::Internet.url,
-	city: 'Denver',
-	state: 'CO',
-	street: '410 E 7th Ave, Denver',
-	zipcode: 80_203,
-	theme: Faker::Restaurant.type,
-	description: Faker::Restaurant.description,
-	lat: 19.1178284578,
-	long: 74.7352457419
-)
-
-Customer.create!(user_id: 1, custname: 'Athos')
-
-Customer.create!(user_id: 2, custname: 'Porthos')
-
-Customer.create!(user_id: 3, custname: 'Aramis')
-
-@business_id = 0
-3.times do |i|
-	@business_id += 1
-	name = Faker::Company.bs
-	Menu.create!(business_id: @business_id, name: name)
-end
-
-10.times do |i|
-	item_name = Faker::Commerce.product_name
-	price = Faker::Commerce.price
-	Item.create!(
-		menu_id: 1,
-		item_name: item_name,
-		price: price,
-		inventory: 5,
-		category: 'Breakfast, Lunch and Dinner.'
+(1..15).each do |i|
+	User.create!(
+		username: Faker::Internet.user_name,
+		email: Faker::Internet.email,
+		password: Faker::Internet.password
 	)
 end
 
-10.times do |i|
-	item_name = Faker::Commerce.product_name
-	price = Faker::Commerce.price
-	Item.create!(
-		menu_id: 2,
-		item_name: item_name,
-		price: price,
-		inventory: 5,
-		category: 'Breakfast, Lunch and Dinner.'
+# Cleanup Existing Business Data
+Business.delete_all
+
+# 5 businesses with user_ids 1-5
+(1..10).each do |i|
+	Business.create!(
+		user_id: i,
+		name: Faker::Restaurant.name,
+		website: Faker::Internet.url,
+		city: 'Denver',
+		state: 'CO',
+		street: real_places[i][:street],
+		zipcode: real_places[i][:zip],
+		theme: Faker::Restaurant.type,
+		description: Faker::Restaurant.description,
+		lat: real_places[i][:lat],
+		long: real_places[i][:long]
 	)
 end
 
-10.times do |i|
-	item_name = Faker::Commerce.product_name
-	price = Faker::Commerce.price
+# Cleanup Existing Menu Data
+Menu.delete_all
+
+# 5 menus with business_id 1-5
+(1..10).each { |i| Menu.create!(business_id: i, name: Faker::Company.bs) }
+
+# Cleanup Existing Customer Data
+Customer.delete_all
+
+# 5 customers with user_ids 6-10
+(11..15).each do |i|
+	Customer.create!(user_id: i, custname: Faker::DcComics.name)
+end
+
+# Cleanup Existing Item Data
+Item.delete_all
+
+(1..10).each do |i|
+	# add more calls to create for additional menu items
+	# number of .create calls == number of items per menu
 	Item.create!(
-		menu_id: 3,
-		item_name: item_name,
-		price: price,
-		inventory: 5,
-		category: 'Breakfast, Lunch and Dinner.'
+		menu_id: i,
+		item_name: Faker::Food.dish,
+		price: Faker::Commerce.price,
+		inventory: Faker::Number.within(range: 1..10),
+		category: Faker::Restaurant.type
+	)
+	Item.create!(
+		menu_id: i,
+		item_name: Faker::Food.dish,
+		price: Faker::Commerce.price,
+		inventory: Faker::Number.within(range: 1..10),
+		category: Faker::Restaurant.type
 	)
 end
 
-Review.create!(
-	customer_id: 1,
-	business_id: 1,
-	review:
-		"The food was way too salty and the chicken tasted like dumpster fish.... I can't wait to go back!!!",
-	rating: 5
-)
+# Cleanup Existing Review Data
+Review.delete_all
 
-Review.create!(
-	customer_id: 2,
-	business_id: 1,
-	review:
-		'The price was cheap, the portions were generous, and the quality was beyond perfect. Never going back. Perfection is overrated.',
-	rating: 1
-)
+# have each customer leave 1 review on each business, add more reviews as needed with extra .create calls
 
-Review.create!(
-	customer_id: 3,
-	business_id: 1,
-	review:
-		"I spilled coffee on myself. It was hot and did not feel good. If it weren't for that, I would have rated them a 5.",
-	rating: 3
-)
+(1..10).each do |i|
+	Review.create!(
+		customer_id: 1,
+		business_id: i,
+		review: Faker::Restaurant.review,
+		rating: Faker::Number.within(range: 1..5)
+	)
+	Review.create!(
+		customer_id: 2,
+		business_id: i,
+		review: Faker::Restaurant.review,
+		rating: Faker::Number.within(range: 1..5)
+	)
+	Review.create!(
+		customer_id: 3,
+		business_id: i,
+		review: Faker::Restaurant.review,
+		rating: Faker::Number.within(range: 1..5)
+	)
+	Review.create!(
+		customer_id: 4,
+		business_id: i,
+		review: Faker::Restaurant.review,
+		rating: Faker::Number.within(range: 1..5)
+	)
+	Review.create!(
+		customer_id: 5,
+		business_id: i,
+		review: Faker::Restaurant.review,
+		rating: Faker::Number.within(range: 1..5)
+	)
+end
 
-Review.create!(
-	customer_id: 1,
-	business_id: 2,
-	review:
-		'The grandma-aged woman who served me coffee called me sweetheart. My girlfriend got jealous and broke up with me...',
-	rating: 1.5
-)
-
-Review.create!(
-	customer_id: 2,
-	business_id: 2,
-	review:
-		'The dentures of the grandma-aged woman, who served me, fell into my coffee. It still tasted fantastic!',
-	rating: 5
-)
-
-Review.create!(
-	customer_id: 3,
-	business_id: 2,
-	review:
-		"I didn't find out there was a cockroach in my soup until it started moving after being bitten in half!",
-	rating: 5
-)
-
-Review.create!(
-	customer_id: 1,
-	business_id: 3,
-	review:
-		'I ordered pancakes, and got waffles... I like waffles more than pancakes.',
-	rating: 5
-)
-
-Review.create!(
-	customer_id: 2,
-	business_id: 3,
-	review:
-		'I tripped on flat ground as I was walking to my table... It made me uncomfortable...',
-	rating: 1
-)
-
-Review.create!(
-	customer_id: 3,
-	business_id: 3,
-	review: 'Another customer said that my food looked delicious...',
-	rating: 2.3
-)
