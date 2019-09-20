6ba0a29e7700dd58a5eaa6923c802af2b2a4ce0c
Julian Kohlman <juliankohlman@gmail.com>
2019-09-20 14:32:52 -0400

    search update

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,14 +2,14 @@ module Api
 	module V1
 		class SearchController < ApplicationController
 			def search
-				# ! Error checking some restaurant names will have numbers, but no
-				# ! symbols in query param regex should kick out symbols
-				businesses = Business.search_by_theme(params[:query])
-				items = Business.search_menu_for_item(params[:query])
-				results = [businesses, items]
-
-				#! Todo ERROR CHECKING AND TESTS
-				render json: results.to_json
+			
+				businesses = Business.search_by_theme(params[:query]).ids
+        items = Business.search_menu_for_item(params[:query]).ids
+        
+        results = Business.where(id: businesses + items).distinct
+      
+        render json: results
+        
 			end
 		end
 	end
